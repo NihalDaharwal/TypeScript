088fd0ca7d84723afe68a91f911ff96f61a46eb4
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-03 16:07:47 +0530

    filesadded: enum.ts and any.ts

---
+++ Ts2/010Any.ts
@@ -0,0 +1,2 @@
+let a: any = 10;
+//do not use any if possible
---
+++ Ts2/09Enum.ts
@@ -0,0 +1,12 @@
+// a group of constant
+// that can assign a number to your string and make a easy comparison
+//  syntax  example => enum Role {value}
+
+enum Role {
+  ADMIN,
+  MANAGER,
+  READ_ONLY_USER,
+}
+
+console.log(Role);
+console.log(Role.MANAGER);
