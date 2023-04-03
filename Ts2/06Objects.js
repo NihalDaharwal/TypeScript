17c2026e76ba77d600ef20d0abea24aebbb68cbb
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-03 13:27:10 +0530

    directory changed of objects.ts to Ts2

---
+++ Ts2/06Objects.js
@@ -0,0 +1,40 @@
+//normal objects
+// const person = {
+//   name: "nihal",
+//   age: 27,
+// };
+// console.warn(person);
+//with ts
+var person = {
+    name: "nihal",
+    age: 27
+};
+console.warn(person); //will not show error because it behave like blank objects {}
+// console.warn(person.name); // shows error
+//or
+var person1 = {
+    name: "nihal",
+    age: 27
+};
+console.warn(person1);
+//
+var person2 = {
+    name: "nihal",
+    age: 27
+};
+console.warn(person2.name);
+var user = {
+    name: "nihal",
+    age: 27
+};
+console.warn(user.name);
+var person3 = {
+    name: "nihal",
+    age: 27
+};
+console.warn(person2.name);
+var user1 = {
+    name: "nihal",
+    age: 27
+};
+console.warn(user.name);
