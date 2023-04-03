ab1783f234ab7ba7fa80250cc40fb5abe3d0ce68
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-03 11:14:15 +0530

    ts vs js file added

---
+++ Ts2/04Ts_vs_js.ts
@@ -0,0 +1,28 @@
+// in js
+
+// function sum(num1, num2) {
+//   if (typeof num1 == "number" && typeof num2 == "number") {
+//     return num1 + num2;
+//   } else {
+//     return new Error("values are not number");
+//   }
+// }
+// console.warn(sum("10", 30)); // show error
+
+// function sum(num1, num2) {
+//     if (typeof num1 == "number" && typeof num2 == "number") {
+//       return num1 + num2;
+//     } else {
+//       return new Error("values are not number");
+//     }
+//   }
+//   console.warn(sum(20, 30)); // output 50
+
+// in ts
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+let result = add(10, 20);
+console.warn(result);
