8163336b8608865f6e3480e5804762f9d517642f
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-03 10:35:07 +0530

    added examples in core types

--- Ts2/03coretypes.ts
+++ Ts2/03coretypes.ts
@@ -7,3 +7,17 @@ let num: number = 10;
 //or
 let num1: number;
 num1 = 10;
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// let result = add("10", 20); show error because n1 type is number number and we wrote string there
+let result = add(10, 20);
+console.warn(result);
+
+function dummy(a: string) {
+  return a;
+}
+let result1 = dummy("hello");
+console.warn(result1);
