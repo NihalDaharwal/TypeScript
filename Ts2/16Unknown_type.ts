707eb9d5afc7b8153c9238753fbed17ad910798b
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-04 12:37:07 +0530

    file: unknowntype.ts added

---
+++ Ts2/16Unknown_type.ts
@@ -0,0 +1,19 @@
+// unknown type
+
+// any is used ts is disable its power
+let data: any;
+data = 20;
+data = "hello";
+
+let item: string;
+item = data;
+
+//unknown is used
+let data1: unknown;
+data1 = 20;
+data1 = "hello";
+
+let item1: string;
+// item = data1; // will show error
+
+//use unknown instead of any
