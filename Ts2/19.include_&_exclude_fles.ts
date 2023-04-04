638f2e3e021162f1ed90a1277cc0569406f01a45
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-04 18:26:09 +0530

    include&excludefile.ts modified

--- Ts2/19.include_&_exclude_fles.ts
+++ Ts2/19.include_&_exclude_fles.ts
@@ -2,4 +2,13 @@
 // then at last and add "exclude": []
 //inside it wite filename or folder name to exclude
 
+// example "exclude": ["Ts/05Classes.ts", "Ts2"]
 
+//Ts/05Classes.ts is file name
+//Ts2 is folder name
+
+// */test.ts is used for specific file whole project , file name can be anything
+
+
+// include to for specific file
+// "include": ["Ts/05Classes.ts", "Ts2"]
