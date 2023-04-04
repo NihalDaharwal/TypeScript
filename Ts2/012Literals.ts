18bb2dba5b83d7c1d515384d79762f5963a3ce87
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-04 10:47:08 +0530

    file : literal.ts added

--- Ts2/012Literals.ts
+++ Ts2/012Literals.ts
@@ -2,3 +2,17 @@
 
 // let Data : string | number ="hello"
 // let Data : "abc" |123 ="abc"
+//
+function combine1(
+  a: number | string,
+  b: number | string,
+  type: "as-number" | "as-string"
+) {
+  if (type === "as-number") {
+    return +a + +b;
+  } else {
+    return a.toString() + b.toString();
+  }
+}
+console.warn(combine1(9, 8, "as-number"));
+console.warn(combine1("hello", "vela", "as-number"));
