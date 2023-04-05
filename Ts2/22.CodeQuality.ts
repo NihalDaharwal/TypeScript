3e53120a296e16f614957ae73a4f67d2930242c0
@nihaldaharwal <nsanihaldaharwal@gmail.com>
2023-04-05 12:00:47 +0530

    file: codeQuality.ts added

---
+++ Ts2/22.CodeQuality.ts
@@ -0,0 +1,45 @@
+// to improve the code quality
+// we cam use these options
+// got to tsconfig.json
+// enable or uncomment these options
+
+// "noUnusedLocals": true,
+// "noImplicitReturns": true,
+// "noUnusedParameters": true,
+// "noFallthroughCasesInSwitch": true,
+
+// "noUnusedLocals": true,
+function apple() {
+  let item = 10;
+  return true;
+}
+apple();
+
+//  "noUnusedParameters": true,
+function apple1(data) {
+  // let item = 10;
+  // return true;
+}
+apple1("fruit");
+
+// "noImplicitReturns": true,
+function apple3(data: string | number) {
+  if (data == 20) {
+    return true;
+  } else {
+    console.log("else");
+  }
+}
+apple3("fruit");
+
+// "noFallthroughCasesInSwitch": true,
+function apple4(data: string | number) {
+  switch (data) {
+    case "fruit":
+      console.log("fruit");
+    //   break;
+    case "color":
+      console.log("color");
+  }
+}
+apple4("fruit");
